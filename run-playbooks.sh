3b362ac99734a7f7b0ac80e2690baf8ec16d31be
James Wang <veriathon@gmail.com>
2022-08-02 21:37:44 -0700

    Adding ruby PATH for Apple Silicon

--- run-playbooks.sh
+++ run-playbooks.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Required for Apple Silicon
 export PATH="$HOME/Library/Python/3.8/bin:/opt/homebrew/bin:$PATH"
+export PATH=/opt/homebrew/opt/ruby/bin:$PATH
+export PATH=`gem environment gemdir`/bin:$PATH
+
 ansible-playbook main.yml --ask-become-pass
